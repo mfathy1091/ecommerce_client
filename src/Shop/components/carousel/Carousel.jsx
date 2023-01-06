74efdbfb2d5becf2c39b108aef835b01bd2b45dc
Mohamed Fathy <mohammad.soliman@gmail.com>
2023-01-06 20:04:55 +0200

    change current slide

--- src/Shop/components/carousel/Carousel.jsx
+++ src/Shop/components/carousel/Carousel.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import CarouselItem from './CarouselItem'
 
 const SCarousel = styled.div`
-
+  margin: 0 auto;
+  overflow: hidden;
+  max-width: 900px;
 `
 
 const SCarouselInner = styled.div`
-  
+  white-space: nowrap;
+  transition: ease 1s;
 `
 
 const Carousel = ({ slides }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentSlide(currentSlide => currentSlide < slides.length - 1 ? currentSlide + 1 : 0 );
+      console.log('hleeee');
+    }, 3000)
+
+    return () => clearInterval(slideInterval)
+  }, [])
+
   return (
 
     <SCarousel>
-      <SCarouselInner>
+      <SCarouselInner
+        style={{ transform: `translate(${-currentSlide*100}%)` }}
+      >
         {slides.map((slide, index) => (
-          <CarouselItem  slide={slide} key={index} />
+          <CarouselItem slide={slide} key={index} />
         ))}
       </SCarouselInner>
     </SCarousel>
--- src/Shop/components/carousel/CarouselItem.jsx
+++ src/Shop/components/carousel/CarouselItem.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SCarouselItem = styled.div`
-
+  display: inline-block;
+  width: 100%;
 `
 
 const CarouselItem = ({ slide }) => {
